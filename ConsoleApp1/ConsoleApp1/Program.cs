f3ed26a73a8ff64a0b6b629273490f3ab2aa3722
PetruCosmin <120106436+PetruCosmin@users.noreply.github.com>
2023-03-09 02:22:47 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string D;
+            Date Date1 = new Date();
+                Student stud1 = new Student();
+                stud1.Id = 1;
+                stud1.Name = "Student1";
+            Date1.spe = "TI";
+            D = Date1.spe;
+            stud1.DateUniv =D;
+
+                Console.WriteLine("Id ul studentului este: {0}", stud1.Id);
+                Console.WriteLine("Numele studentului: {0}", stud1.Name);
+                Console.WriteLine("Datele studentului: {0}", stud1.DateUniv);
+        }
+        }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+	public class Student
+	{
+		public Student()
+		{
+		}
+
+		private int studID;
+		private string studNume;
+		private string studDate;
+
+		public int Id
+		{get{return studID;}
+			set	{studID = value;}
+		}
+
+		public string Name
+		{
+			get{return studNume;}
+			set	{studNume = value;}
+
+		}
+		public string DateUniv
+		{
+			get { return studDate; }
+			set { studDate = value; }
+		}
+	}
+}
